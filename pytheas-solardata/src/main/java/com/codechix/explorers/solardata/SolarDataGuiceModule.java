59b754c669cc6ea907fff32e3b816fa02a0e897f
lhurley <lhurley@cj.com>
2014-02-02 13:05:49 -0800

    de-register fake data module. tweak names for remaining explorers

--- pytheas-solardata/src/main/java/com/codechix/explorers/solardata/SolarDataGuiceModule.java
+++ pytheas-solardata/src/main/java/com/codechix/explorers/solardata/SolarDataGuiceModule.java
@@ -11,7 +11,7 @@ public class SolarDataGuiceModule extends AbstractModule {
     protected void configure() {
         Multibinder<Explorer> explorersBinder = Multibinder.newSetBinder(binder(), Explorer.class);
         explorersBinder.addBinding().to(SolarDataExplorer.class);
-        explorersBinder.addBinding().to(FakeDataExplorer.class);
+//        explorersBinder.addBinding().to(FakeDataExplorer.class);
         explorersBinder.addBinding().to(SolarMapExplorer.class);
 
     }
